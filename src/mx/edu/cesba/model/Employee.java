bab1310eeef470cd95dfc3db4c982716547e4c37
LENOVO 330 <LENOVO 330@DESKTOP-16BSVEN>
2021-02-01 19:39:34 -0600

    1.1

---
+++ src/mx/edu/cesba/model/Employee.java
@@ -0,0 +1,20 @@
+/*
+Nombre: Josue Julian Leon Garcia
+Matricula: 2022995
+ */
+package mx.edu.cesba.model;
+
+public class Employee {
+
+    public int id;
+    public String firstName;
+    public String latName;
+    public int age;
+    public int phoneNumber;
+    public double salary;
+    public String hiredate;
+
+    public void getDetails() { //METODO
+
+    }
+}
