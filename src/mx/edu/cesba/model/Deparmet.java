1a8e1a9b363837230c03374d0016285554d9fb65
LENOVO 330 <LENOVO 330@DESKTOP-16BSVEN>
2021-02-01 19:44:08 -0600

    1.2

---
+++ src/mx/edu/cesba/model/Deparmet.java
@@ -0,0 +1,19 @@
+/*
+Nombre: Josue Julian Leon Garcia
+Matricula: 2022995
+ */
+package mx.edu.cesba.model;
+
+/**
+ *
+ * @author LENOVO 330
+ */
+public class Deparmet {
+    public int id;
+    public String title;
+    public String location;
+    
+    public void getDetails() { //METODO
+    
+}
+}
---
+++ src/mx/edu/cesba/model/Job.java
@@ -0,0 +1,20 @@
+/*
+Nombre: Josue Julian Leon Garcia
+Matricula: 2022995
+ */
+package mx.edu.cesba.model;
+
+/**
+ *
+ * @author LENOVO 330
+ */
+public class Job {
+     public int id;
+     public String title;
+     public double minSalary;
+     public double maxSalary;
+     
+     public void getDetails() { //METODO
+    
+}
+}
